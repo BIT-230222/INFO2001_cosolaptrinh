1467e6b96b4e1a00d49f192be91cc317efe2e815
BIT-230222 <148991606+BIT-230222@users.noreply.github.com>
2023-10-30 11:46:02 +0700

    nop btth03

--- btth03/bai1.c
+++ btth03/bai1.c
@@ -1,8 +1,16 @@
-djfhsadcsnv uoef
-uc dfwdcvw
-=fckvadsd f\uefdv
-sdfsdnvs df
-\udfsdkvsdfs
-csdfms 
-vsd
-fsdkfsdvmsdf slsdfwDCVSCV ?
+#include <stdio.h>
+
+int main() {
+    int number;
+    
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &number);
+    
+    if (number % 2 == 0) {
+        printf("%d là so chan.\n", number);
+    } else {
+        printf("%d là so le.\n", number);
+    }
+    
+    return 0;
+}
