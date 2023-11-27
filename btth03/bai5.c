8f78b6b8429511484e1689add5491894e15a8a0d
BIT-230222 <148991606+BIT-230222@users.noreply.github.com>
2023-11-27 23:23:49 +0700

    nop bai

--- btth03/bai5.c
+++ btth03/bai5.c
@@ -24,5 +24,52 @@ int main() {
         printf("%d khong phai là so nguyen to.\n", N);
     }
     
+    
+
+
+ 
+    int number;
+    
+    printf("Nhap mot so nguyen: ");
+    scanf("%d", &number);
+    
+    if (number % 2 == 0) {
+        printf("%d là so chan.\n", number);
+    } else {
+        printf("%d là so le.\n", number);
+    }
+    
+    return 0;
+
+}
+	float a;
+	printf("nhap so diem : ");
+	scanf("%f",&a);
+	if(a<=10&&a>=0)
+		if(a>=9)
+		printf("hoc sinh xuat sac");
+			else if(a>=8)
+			printf("hoc sinh gioi");
+					else if(a>=6.5)
+				printf("hoc sinh kha");
+						else if(a>=5)
+							printf("hoc sinh trung binh");
+								else
+								printf("hoc sinh yeu");
+    return 0;	
+ int main() {
+    int N, i = 1, sum = 0;
+    
+    printf("Nhap so le (N): ");
+    scanf("%d", &N);
+    
+    while (N > 0) {
+        sum += i;
+        i += 2;
+        N--;
+    }
+    
+    printf("Tong cua %d so le dau tien là: %d\n", N, sum);
+    
     return 0;
 }
